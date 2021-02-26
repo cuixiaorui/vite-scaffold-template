a1dbb9b3305cc935b9ed7aef25e4fe3b01adc9f7
cuixiaorui <cui_xiaorui@126.com>
2021-02-26 16:58:01 +0800

    test: change e2e url

--- tests/e2e/specs/test.ts
+++ tests/e2e/specs/test.ts
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Does not do much!', () => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:5000/')
     cy.get('button').click()
   })
 })
