9bae921ca50e96f286d28bebd7f95bc2a79893e3
cuixiaorui <cui_xiaorui@126.com>
2021-03-01 19:05:25 +0800

    feat: setup e2e test

--- tests/e2e/specs/first.spec.ts
+++ tests/e2e/specs/first.spec.ts
@@ -1,5 +1,5 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
+describe('first', () => {
+  it('one', () => {
     cy.visit('http://localhost:3000/')
     cy.get('button').click()
   })
