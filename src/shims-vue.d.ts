ceb7949c07669c3605fc589508eacac0c6f0b9f1
cuixiaorui <cui_xiaorui@126.com>
2021-03-04 12:15:53 +0800

    chore: ignore ts lint

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
